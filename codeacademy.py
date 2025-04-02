e8a574a1138bc8404b70a886eff1119cc740756b
camilabinnie <binnieartist@gmail.com>
2025-04-02 01:51:03 -0300

    studying

---
+++ codeacademy.py
@@ -0,0 +1,14 @@
+# 01
+my_name = "Binnie"
+print ("Hello cutie " + my_name + "S2")
+
+# from Mary Shelley's Frankenstein
+print("There is something at work in my soul, which I do not understand.")
+
+#Create a program that asks the user to enter their name and their age.
+#  Print out a message addressed to them that tells them the year that 
+# they will turn 100 years old
+user_name = input("What is your name:")
+user_age = int(input("How old are you:"))
+year = 2025 - user_age + 100
+print(f"{user_name}, you will be 100 years old in the year {year}")
